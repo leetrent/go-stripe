f4184c6d54042cf1c163452d41fb0001c7b46acc
LeeTrent <lee.trent.1@gmail.com>
2022-01-08 11:45:10 -0600

    Using go templates to render virtual terminal page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,4 +4,8 @@ import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	app.infoLog.Println("[application][VirtualTerminal] => ()")
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println("[application][VirtualTerminal] => (error encounterd):")
+		app.errorLog.Println(err)
+	}
 }
