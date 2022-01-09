99cb3918c74842db4b1f07f42566f7d720eceaf9
LeeTrent <lee.trent.1@gmail.com>
2022-01-09 10:31:22 -0600

    Reorganized instructor's template processing code

--- cmd/web/application.go
+++ cmd/web/application.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 )
+
+var functions = template.FuncMap{}
+
+//go:embed templates
+var templateFS embed.FS
 
 type application struct {
 	config        config
@@ -16,6 +23,20 @@ type application struct {
 	errorLog      *log.Logger
 	templateCache map[string]*template.Template
 	version       string
+}
+
+type templateData struct {
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	IsAuthenticated int
+	API             string
+	CSSVersion      string
 }
 
 func (app *application) serve() error {
@@ -33,6 +54,66 @@ func (app *application) serve() error {
 	return srv.ListenAndServe()
 }
 
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	return td
+}
+
+func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
+	var t *template.Template
+	var err error
+	templateToRender := fmt.Sprintf("templates/%s.page.gohtml", page)
+	_, templateInMap := app.templateCache[templateToRender]
+
+	if app.config.env == "production" && templateInMap {
+		t = app.templateCache[templateToRender]
+	} else {
+		t, err = app.parseTemplate(partials, page, templateToRender)
+		if err != nil {
+			app.errorLog.Println(err)
+			return err
+		}
+	}
+
+	if td == nil {
+		td = &templateData{}
+	}
+
+	td = app.addDefaultData(td, r)
+
+	err = t.Execute(w, td)
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (app *application) parseTemplate(partials []string, page string, templateToRender string) (*template.Template, error) {
+	var t *template.Template
+	var err error
+
+	// builf partials
+	if len(partials) > 0 {
+		for idx, val := range partials {
+			partials[idx] = fmt.Sprintf("templates/%s.partial.gohtml", val)
+		}
+	}
+
+	if len(partials) > 0 {
+		t, err = template.New(fmt.Sprintf("%s.page.gohtml", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.gohtml", strings.Join(partials, ","), templateToRender)
+	} else {
+		t, err = template.New(fmt.Sprintf("%s.page.gohtml", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.gohtml", templateToRender)
+	}
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return nil, err
+	}
+
+	app.templateCache[templateToRender] = t
+	return t, nil
+}
+
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
@@ -42,4 +123,8 @@ func (app *application) routes() http.Handler {
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	app.infoLog.Println("[application][VirtualTerminal] => ()")
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println("[application][VirtualTerminal] => (error encounterd):")
+		app.errorLog.Println(err)
+	}
 }
