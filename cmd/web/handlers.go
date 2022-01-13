3a91d675c1647d76ed2e52d9cce2cc93d97b89dd
LeeTrent <lee.trent.1@gmail.com>
2022-01-13 10:08:00 -0600

    Built credit card transaction succeeded page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,3 +9,33 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 		app.errorLog.Println(err)
 	}
 }
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// Read posted data
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := make(map[string]interface{})
+	data["cardHolder"] = cardHolder
+	data["email"] = email
+	data["paymentIntent"] = paymentIntent
+	data["paymentMethod"] = paymentMethod
+	data["paymentAmount"] = paymentAmount
+	data["paymentCurrency"] = paymentCurrency
+
+	if err := app.renderTemplate(w, r, "succeeded", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
