d367f0a44dec5cddbd3bebfd452f6041f051dbdb
LeeTrent <lee.trent.1@gmail.com>
2022-01-15 10:38:20 -0600

    Added buy-once page using stripe-js partial template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,7 +12,7 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 
 	if err := app.renderTemplate(w, r, "terminal", &templateData{
 		StringMap: stringMap,
-	}); err != nil {
+	}, "stripe-js"); err != nil {
 		app.errorLog.Println("[application][VirtualTerminal] => (error encounterd):")
 		app.errorLog.Println(err)
 	}
@@ -49,7 +49,11 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 }
 
 func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "buy-once", nil); err != nil {
+	stringMap := make(map[string]string)
+	stringMap["publishable_key"] = app.config.stripe.key
+
+	if err := app.renderTemplate(w, r, "buy-once", 
+	&templateData{StringMap: stringMap,}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 	}
 }
