c13c0236c72e2a9e88e62ff6946cdbf1ad1ef2bc
LeeTrent <lee.trent.1@gmail.com>
2022-01-17 12:04:02 -0600

    Working buy once page using newly created widget model

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/leetrent/go-stripe/internal/models"
+)
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	logSnippet := "[application][VirtualTerminal] =>"
@@ -49,11 +53,25 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 }
 
 func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
+	widget := models.Widget{
+		ID:             1,
+		Name:           "Custom Widget",
+		Description:    "A very nice widget",
+		InventoryLevel: 10,
+		Price:          1000,
+	}
+
+	data := make(map[string]interface{})
+	data["widget"] = widget
+
 	stringMap := make(map[string]string)
 	stringMap["publishable_key"] = app.config.stripe.key
 
-	if err := app.renderTemplate(w, r, "buy-once", 
-	&templateData{StringMap: stringMap,}, "stripe-js"); err != nil {
+	if err := app.renderTemplate(w, r, "buy-once",
+		&templateData{
+			StringMap: stringMap,
+			Data:      data,
+		}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 	}
 }
