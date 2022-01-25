571b2e9b398b2d263f31ecbe3836ea95008fceed
LeeTrent <lee.trent.1@gmail.com>
2022-01-25 10:38:11 -0600

    Starting bronze subscription page

--- internal/models/models.go
+++ internal/models/models.go
@@ -31,6 +31,8 @@ type Widget struct {
 	InventoryLevel int       `json:"inventory_level"`
 	Price          int       `json:"price"`
 	Image          string    `json:"image"`
+	IsRecurring    bool      `json:"is_recurring"`
+	PlanID         string    `json:"plan_id"`
 	CreatedAt      time.Time `json:"-"`
 	UpdatedAt      time.Time `json:"-"`
 }
@@ -114,6 +116,8 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 				inventory_level,
 				price,
 				coalesce(image, ''),
+				is_recurring,
+				plan_id,
 				created_at,
 				updated_at
 		 FROM	widgets 
@@ -126,6 +130,8 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 		&widget.InventoryLevel,
 		&widget.Price,
 		&widget.Image,
+		&widget.IsRecurring,
+		&widget.PlanID,
 		&widget.CreatedAt,
 		&widget.UpdatedAt)
 
