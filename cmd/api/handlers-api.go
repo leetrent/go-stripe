081033f9df407f37517f0c2e546e902a0a9832cd
LeeTrent <lee.trent.1@gmail.com>
2022-02-11 10:36:37 -0600

    Wiring up sales detail page

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -614,3 +614,23 @@ func (app *application) AllSubscriptions(w http.ResponseWriter, r *http.Request)
 
 	app.writeJSON(w, http.StatusOK, allRecurringSales)
 }
+
+func (app *application) GetSale(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	orderID, err := strconv.Atoi(id)
+	if err != nil {
+		app.errorLog.Println(err)
+		app.badRequest(w, r, err)
+		return
+	}
+
+	order, err := app.DB.GetOrderByID(orderID)
+	if err != nil {
+		app.errorLog.Println(err)
+		app.badRequest(w, r, err)
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, order)
+}
